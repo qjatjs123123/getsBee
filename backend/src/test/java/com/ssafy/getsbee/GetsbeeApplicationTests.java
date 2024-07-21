9bddc3f4aee71f3f096e849488b4826a2df37066
hoshogi <hoshogi@gmail.com>
2024-07-21 19:11:48 +0900

    feat: 프로젝트 생성

---
+++ backend/src/test/java/com/ssafy/getsbee/GetsbeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.getsbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class GetsbeeApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
