7422176c2ebc16001aae469bc5fc01229607b500
hoshogi <hoshogi@gmail.com>
2024-07-22 08:43:59 +0900

    feat: 회원 조회 기능 구현
    - SecurityUtil 기본 코드 추가

---
+++ backend/src/main/java/com/ssafy/getsbee/global/security/SecurityUtil.java
@@ -0,0 +1,11 @@
+package com.ssafy.getsbee.global.security;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class SecurityUtil {
+
+    public static Long getCurrentMemberId() {
+        return 1L;
+    }
+}
