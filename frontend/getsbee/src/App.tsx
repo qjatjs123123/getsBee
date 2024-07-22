43bf8656294bafe9ff2ccd7c22771f92a41dcd16
chanhyun22 <bch0707@dongguk.edu>
2024-07-22 16:23:47 +0900

    feat: 전체적인 페이지 생성
    - Home 페이지
    - Recommend 페이지
    - MyHive 페이지
    - About 페이지

--- frontend/getsbee/src/App.tsx
+++ frontend/getsbee/src/App.tsx
@@ -1,8 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Recommend from './pages/Recommend';
+import MyHive from './pages/MyHive';
+import About from './pages/About';
+
 function App() {
   return (
-    <div>
-      <h1>123</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recommend" element={<Recommend />} />
+      <Route path="/myhive" element={<MyHive />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
