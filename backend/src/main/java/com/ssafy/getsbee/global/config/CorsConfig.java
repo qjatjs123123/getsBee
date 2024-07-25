c87476affd489fef8c971756c59b3e84732af045
hoshogi <hoshogi@gmail.com>
2024-07-25 16:55:11 +0900

    feat: CorsConfig 수정
    - max age 수정
    - expose header 설정

--- backend/src/main/java/com/ssafy/getsbee/global/config/CorsConfig.java
+++ backend/src/main/java/com/ssafy/getsbee/global/config/CorsConfig.java
@@ -1,9 +1,14 @@
 package com.ssafy.getsbee.global.config;
 
+import com.ssafy.getsbee.global.consts.StaticConst;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+
+import static com.ssafy.getsbee.global.consts.StaticConst.*;
 
 public class CorsConfig {
 
@@ -16,6 +21,8 @@ public class CorsConfig {
         config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
+        config.setExposedHeaders(Collections.singletonList(AUTHORIZATION_HEADER));
 
         configSource.registerCorsConfiguration("/**", config);
         return new CorsFilter(configSource);
