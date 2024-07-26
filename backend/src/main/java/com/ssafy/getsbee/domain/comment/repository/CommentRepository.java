518775f04cbddf17c565ba955c5e47e2ddee1bd3
이호석 <hoshogi@gmail.com>
2024-07-26 16:53:22 +0900

    Revert "Merge branch 'backend/feature/13' into 'main'"
    This reverts merge request !11

--- backend/src/main/java/com/ssafy/getsbee/domain/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.getsbee.domain.comment.repository;
-
-import com.ssafy.getsbee.domain.comment.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- backend/src/main/java/com/ssafy/getsbee/domain/highlight/repository/HighlightRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.getsbee.domain.highlight.repository;
-
-import com.ssafy.getsbee.domain.highlight.entity.Highlight;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HighlightRepository extends JpaRepository<Highlight, Long> {
-}
