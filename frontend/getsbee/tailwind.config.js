5fa5ca7163420794500ee5d8c346dadf2d8b95b8
chanhyun22 <bch0707@dongguk.edu>
2024-07-26 17:35:20 +0900

    feat: Roboto 폰트적용
    - Roboto 폰트적용

--- frontend/getsbee/tailwind.config.js
+++ frontend/getsbee/tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: 100,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        bold: 700,
+        black: 900,
+      },
+    },
   },
   plugins: [],
 };
