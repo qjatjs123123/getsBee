05bbb54c4996f6aa9fb052dab8bd0960a078a1b0
Myeonghwa Kim <mhwazz@gmail.com>
2024-07-31 14:01:16 +0900

    feat: 컴색바 및 추천 컴포넌트 구현
    - 검색바 구현
    - 추천 컴포넌트 구현

--- frontend/getsbee/src/components/Common/MainSearchBar.tsx
+++ frontend/getsbee/src/components/Common/MainSearchBar.tsx
@@ -1,12 +1,38 @@
+import React, { useState, FormEvent, ChangeEvent } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
 import searchIcon from '../../assets/searchIcon.png';
-import './MainSearchBar.css';
 
-const MainSearchBar = () => {
+const MainSearchBar: React.FC = () => {
+  const [value, setValue] = useState<string>('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // submit 로직 추가 필요
+    console.log('Submitted value:', value);
+    // 제출 후 입력 필드를 초기화
+    setValue('');
+  };
+
   return (
-    <div className="mainSearchBar">
-      <input className="searchBox" placeholder="search" />
-      <img className="searchIcon" src={searchIcon} alt="searchIcon" />
-    </div>
+    <form onSubmit={handleSubmit} className="flex items-center">
+      <InputText
+        value={value}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+        placeholder="Search"
+        className="border-0 border-b-2 border-gray-300 w-1/2 text-center focus:border-yellow-500 focus:ring-0 bg-transparent rounded-none mr-2"
+      />
+      <Button
+        type="submit"
+        label=""
+        text
+        severity="secondary"
+        aria-label="Bookmark"
+        className="focus:outline-none focus:shadow-none m-0 p-0"
+      >
+        <img src={searchIcon} alt="Search Icon" className="w-10 h-10" />
+      </Button>
+    </form>
   );
 };
 
