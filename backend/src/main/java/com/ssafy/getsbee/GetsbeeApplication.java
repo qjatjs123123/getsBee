9362a9f0a38975b0abe864a02bf46a7c9d2a11e1
dahyunko <kdhgby@naver.com>
2024-08-06 09:19:24 +0900

    update : elastic config 파일 수정

--- backend/src/main/java/com/ssafy/getsbee/GetsbeeApplication.java
+++ backend/src/main/java/com/ssafy/getsbee/GetsbeeApplication.java
@@ -2,11 +2,13 @@ package com.ssafy.getsbee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableConfigurationProperties
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class GetsbeeApplication {
 
