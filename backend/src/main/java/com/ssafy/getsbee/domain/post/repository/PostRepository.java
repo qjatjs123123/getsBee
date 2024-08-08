3c29747ef762c102ec82d1cc753df1265aef8f2e
dahyunko <kdhgby@naver.com>
2024-08-08 13:05:41 +0900

    feat : 동시성 테스트

--- backend/src/main/java/com/ssafy/getsbee/domain/post/repository/PostRepository.java
+++ backend/src/main/java/com/ssafy/getsbee/domain/post/repository/PostRepository.java
@@ -3,14 +3,17 @@ package com.ssafy.getsbee.domain.post.repository;
 import com.ssafy.getsbee.domain.directory.entity.Directory;
 import com.ssafy.getsbee.domain.member.entity.Member;
 import com.ssafy.getsbee.domain.post.entity.Post;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Post> findByMemberAndUrl(Member member, String url);
 
     Optional<Post> findById(Long postId);
