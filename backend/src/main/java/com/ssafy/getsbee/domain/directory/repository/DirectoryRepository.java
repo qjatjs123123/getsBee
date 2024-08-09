0bfb733c4631079b6b578d8c61932e48a8e590a2
dahyunko <kdhgby@naver.com>
2024-08-09 12:38:40 +0900

    refacor : 파일 구조 보낼 때 포스트 수 까지 보내도록 api수정

--- backend/src/main/java/com/ssafy/getsbee/domain/directory/repository/DirectoryRepository.java
+++ backend/src/main/java/com/ssafy/getsbee/domain/directory/repository/DirectoryRepository.java
@@ -2,7 +2,9 @@ package com.ssafy.getsbee.domain.directory.repository;
 
 import com.ssafy.getsbee.domain.directory.entity.Directory;
 import com.ssafy.getsbee.domain.member.entity.Member;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +16,5 @@ public interface DirectoryRepository extends JpaRepository<Directory, Long>, Dir
     Optional<Directory> findDirectoryById(Long directoryId);
 
     List<Directory> findAllByMember(Member member);
+
 }
