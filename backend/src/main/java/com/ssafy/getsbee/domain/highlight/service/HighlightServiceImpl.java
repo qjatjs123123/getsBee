5ef80cd9bc21be53cbc1b93278eb074f7bbbddc9
DESKTOP-6ABJ64L\SSAFY <shinwc1998@naver.com>
2024-08-13 15:54:40 +0900

    feat : S3 본문 내용 저장 refactor

--- backend/src/main/java/com/ssafy/getsbee/domain/highlight/service/HighlightServiceImpl.java
+++ backend/src/main/java/com/ssafy/getsbee/domain/highlight/service/HighlightServiceImpl.java
@@ -161,6 +161,7 @@ public class HighlightServiceImpl implements HighlightService {
                 .orElseThrow(() -> new BadRequestException(HIGHLIGHT_NOT_FOUND));
         Post post = highlight.getPost();
         saveMessageToS3(modifyHighlightBodyResponse.message(), post);
+        postRepository.save(post);
         return S3UrlResponse.from(post.getBodyUrl());
     }
 
